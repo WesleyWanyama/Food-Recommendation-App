4f42cc7125efeb9644c10aecc994e58e0af3d356
WesleyWanyama <83776940+WesleyWanyama@users.noreply.github.com>
2022-11-17 13:14:57 +0300

    ff

---
+++ FoodRecommendationApp/app/src/main/java/com/example/foodrecommendationapp/DiabeticActivity.kt
@@ -0,0 +1,11 @@
+package com.example.foodrecommendationapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DiabeticActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diabetic)
+    }
+}
