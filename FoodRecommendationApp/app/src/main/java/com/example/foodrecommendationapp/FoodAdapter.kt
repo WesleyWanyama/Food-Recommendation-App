4f93da00dbcb9de8d49e004c80c49ef450689d87
WesleyWanyama <83776940+WesleyWanyama@users.noreply.github.com>
2022-11-21 20:31:10 +0300

    Update FoodAdapter.kt

--- FoodRecommendationApp/app/src/main/java/com/example/foodrecommendationapp/FoodAdapter.kt
+++ FoodRecommendationApp/app/src/main/java/com/example/foodrecommendationapp/FoodAdapter.kt
@@ -3,31 +3,43 @@ package com.example.foodrecommendationapp
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class FoodAdapter(val mList: List<Food>):RecyclerView.Adapter<FoodAdapter.ViewHolder>(){
-    class ViewHolder {
-
-    }
+class FoodAdapter(val mList: List<Food>):RecyclerView.Adapter<FoodAdapter.FoodViewHolder>(){
+    //inner class creates the views to be populated with data received from the adapter class
+    inner class FoodViewHolder(itemView: View):RecyclerView.ViewHolder(itemView)
+    /*inner class ViewHolder {
+        val foodImage: ImageView = itemView.findViewById(R.id.foodImage)
+        val foodText: TextView = itemView.findViewById(R.id.foodText)
+    }*/
 
     //Override abstract methods
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): FoodAdapter.ViewHolder{
+    ): FoodAdapter.FoodViewHolder{
 
         //Inflate food info
         val view = LayoutInflater.from(parent.context).inflate(R.layout.food_items, parent, false)
 
-        return ViewHolder(view)
+        return FoodViewHolder(view)
     }
 
     //Binds the list items to a view
-    override fun onBindViewHolder(holder: FoodAdapter.ViewHolder, position: Int) {
-        
+    override fun onBindViewHolder(holder: FoodAdapter.FoodViewHolder, position: Int) {
+        var foodImage: ImageView = holder.itemView.findViewById(R.id.foodImage)
+        var foodText: TextView = holder.itemView.findViewById(R.id.foodText)
+
+        val Food = mList[position]
+
+        foodImage.setImageResource(Food.image)
+
+        foodText.text = Food.text
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return mList.size
     }
 }
